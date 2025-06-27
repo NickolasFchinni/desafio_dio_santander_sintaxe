2ebb10acea8284736a69f5854111871dce33b169
Nickolas <nulleyson@gmail.com>
2025-06-27 03:59:25 -0300

    feat:projeto finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Qual a agência da sua conta: ");
+        String agenciaConta = scanner.nextLine();
+
+        System.out.println("Qual o número da sua conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Qual seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.printf("Olá , %s obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d " +
+                "e seu saldo %.2f já está disponível para saque", nome, agenciaConta, numeroConta, saldo);
+
+        scanner.close();
     }
 }
